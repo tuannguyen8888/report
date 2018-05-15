e1ef7c592ed67a6fe9d390d4a6e3ccba8e6c8b47
Võ Tuấn Nguyên <tuannguyen8888@gmail.com>
2018-05-16 00:56:09 +0700

    edit

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 
 var express = require('express');
 var app = express();
-app.listen(80, function () {
+app.listen(8080, function () {
   console.log('app started');
 });
 var {google} = require('googleapis');
